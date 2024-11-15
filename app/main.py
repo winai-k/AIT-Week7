568e56c0842785bb2fb45c31c50137405834f008
worrarat <panworrarat@gmail.com>
2024-11-15 13:09:43 +0700

    add device params

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ async def model_class_info():
 @app.post("/api/v1/detect/{class_id}")
 async def detection(image: UploadFile, 
                     conf: float = Query(0.8, title="confidence threshold", gt=0), 
+                    device: str = Query("cpu", title="device for inference (cpu, 0, 1)"),
                     class_id: int = Path(..., title="class id of object")):
     """
     ## bbox format = x1, y1, x2, y2
@@ -23,7 +24,7 @@ async def detection(image: UploadFile,
     img = image.file.read()
     np_array = np.frombuffer(img, np.uint8)
     img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-    results = model.predict(img, conf=conf, show_labels=True, device="cpu", vid_stride=3, verbose=False)
+    results = model.predict(img, conf=conf, show_labels=True, device=device, vid_stride=3, verbose=False)
     for result in results:
         for box in result.boxes:
             x1, y1, x2, y2 = box.xyxy.squeeze().tolist()
